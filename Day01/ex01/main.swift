922812a98b98f45d407433c00b0a75bc430d23c9
Kuzivakwashe MUVEZWA <kmuvezwa@e5r11p2.wethinkcode.co.za>
2018-10-03 16:46:45 +0200

    Completed Day01 and Minor Edits to Day00

---
+++ Day01/ex01/main.swift
@@ -0,0 +1,13 @@
+print("Starting Test File...\n");
+
+let c1 = Card(color: Color.Heart, value: Value.Ace);
+let c2 = Card(color: Color.Spade, value: Value.King);
+//let c2 = Card(color: Color.Heart, value: Value.Ace);
+
+print("Card 1 color \(c1.cardColor) and value \(c1.cardValue)");
+print("Card 2 color \(c2.cardColor) and value \(c2.cardValue)");
+print(c1);
+print(c2);
+print(c1 == c2);
+
+print("\nFinished Tests");
